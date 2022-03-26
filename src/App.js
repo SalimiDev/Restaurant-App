7fc14ee1f0571f5defa2eec7a2e3e0d023fb748a
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-03-26 23:51:52 +0430

    feat:created navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//Style
 import './App.css';
+//Component
+import Navbar from './components/Navbar';
 
 function App() {
-    return <div className='App'>Simple Resturant ReactJs Project</div>;
+    return (
+        <div className='App'>
+            <Router>
+                <Navbar />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
+//style & icons
+import '../styles/Navbar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
+
+const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleNavbar = () => {
+        setShowMenu(!showMenu);
+    };
+    return (
+        <div className='navbar'>
+            <div className='leftSide' id={showMenu ? 'open' : 'close'}>
+                <img src={logo} alt='logo' />
+                <div className='hiddenLinks'>
+                    <Link to='./'>Home</Link>
+                    <Link to='./'>Menu</Link>
+                    <Link to='./'>About</Link>
+                    <Link to='./'>Contact</Link>
+                </div>
+            </div>
+            <div className='rightSide'>
+                <Link to='./'>Home</Link>
+                <Link to='./'>Menu</Link>
+                <Link to='./'>About</Link>
+                <Link to='./'>Contact</Link>
+                <button onClick={toggleNavbar}>
+                    <ReorderIcon />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
